17b8619c01170c76b712dd47bb559efeef76fbaa
seekincs <seekincs@gmail.com>
2019-12-06 19:45:17 +0800

    fix LogController.

--- src/main/java/com/tardybird/log/controller/LogController.java
+++ src/main/java/com/tardybird/log/controller/LogController.java
@@ -1,19 +1,45 @@
 package com.tardybird.log.controller;
 
-import com.tardybird.log.service.LogService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.tardybird.log.entity.Ad;
+import com.tardybird.log.entity.Log;
+import com.tardybird.log.service.impl.LogServiceImpl;
+import com.tardybird.log.util.ResponseUtil;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author nick
  */
+@RestController
 public class LogController {
 
-    @Autowired
-    LogService logService;
+    final
+    LogServiceImpl logService;
+
+    public LogController(LogServiceImpl logService) {
+        this.logService = logService;
+    }
 
     @GetMapping("/")
     public Object getAllAds() {
-        return logService.getAllAds();
+        List<Ad> adList = logService.getAllAds();
+        return ResponseUtil.ok(adList);
+    }
+
+    public Object addLog(@RequestBody LogVo logVo) {
+        Log log = new Log();
+        log.setAdminIdId(logVo.getAdminId());
+        log.setIp(logVo.getIp());
+        log.setType(logVo.getType());
+        log.setAction(logVo.getAction());
+        log.setStatusCode(logVo.getStatus());
+        log.setActionId(logVo.getActionId());
+
+        logService.addLog(log);
+        return ResponseUtil.ok();
+
     }
 }
