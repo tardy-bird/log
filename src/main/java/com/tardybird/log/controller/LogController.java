2e6309f9e40cf1244b5f8f6688f4a79027404d63
seekincs <seekincs@gmail.com>
2019-12-08 17:16:27 +0800

    update API URLs.

--- src/main/java/com/tardybird/log/controller/LogController.java
+++ src/main/java/com/tardybird/log/controller/LogController.java
@@ -6,6 +6,7 @@ import com.tardybird.log.service.impl.LogServiceImpl;
 import com.tardybird.log.util.ResponseUtil;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.List;
  * @author nick
  */
 @RestController
+@RequestMapping("/logs")
 public class LogController {
 
     final
@@ -23,22 +25,29 @@ public class LogController {
         this.logService = logService;
     }
 
+    /**
+     * 管理员根据条件查看日志
+     *
+     * @return 所有操作日志
+     */
     @GetMapping("/")
-    public Object getAllAds() {
+    public Object list() {
         List<Ad> adList = logService.getAllAds();
         return ResponseUtil.ok(adList);
     }
 
     public Object addLog(@RequestBody LogVo logVo) {
+
         Log log = new Log();
-        log.setAdminIdId(logVo.getAdminId());
+
+        log.setAdminId(logVo.getAdminId());
         log.setIp(logVo.getIp());
         log.setType(logVo.getType());
         log.setAction(logVo.getAction());
         log.setActionId(logVo.getActionId());
+        log.setStatusCode(logVo.getStatusCode());
 
         logService.addLog(log);
-        return ResponseUtil.ok();
-
+        return ResponseUtil.ok(log);
     }
 }
