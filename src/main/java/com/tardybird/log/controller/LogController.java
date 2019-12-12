db0b427eb670acfdb8018bbf3c470c146aa56d3d
seekincs <seekincs@gmail.com>
2019-12-12 22:19:29 +0800

    update LogController.

--- src/main/java/com/tardybird/log/controller/LogController.java
+++ src/main/java/com/tardybird/log/controller/LogController.java
@@ -1,6 +1,5 @@
 package com.tardybird.log.controller;
 
-import com.tardybird.log.entity.Ad;
 import com.tardybird.log.entity.Log;
 import com.tardybird.log.service.impl.LogServiceImpl;
 import com.tardybird.log.util.ResponseUtil;
@@ -8,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author nick
@@ -30,13 +27,19 @@ public class LogController {
      *
      * @return 所有操作日志
      */
-    @GetMapping("/")
-    public Object list() {
-        List<Ad> adList = logService.getAllAds();
+    @GetMapping("/logs")
+    public Object list(Integer adminId, Integer page, Integer limit) {
+        if (adminId == null || adminId < 0 || page == null || page < 0 || limit == null || limit < 0) {
+            return ResponseUtil.badArgument();
+        }
+        Object adList = logService.getAllAds(adminId, page, limit);
         return ResponseUtil.ok(adList);
     }
 
     public Object addLog(@RequestBody Log log) {
+        if (log == null) {
+            return ResponseUtil.fail();
+        }
         logService.addLog(log);
         return ResponseUtil.ok(log);
     }
