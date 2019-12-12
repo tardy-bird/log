bdcf7941303174fb7fe698d367c5d077e11fb63d
seekincs <seekincs@gmail.com>
2019-12-12 22:19:58 +0800

    remove findAds methods.

--- src/main/java/com/tardybird/log/mapper/LogMapper.java
+++ src/main/java/com/tardybird/log/mapper/LogMapper.java
@@ -1,6 +1,5 @@
 package com.tardybird.log.mapper;
 
-import com.tardybird.log.entity.Ad;
 import com.tardybird.log.entity.Log;
 import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Component;
@@ -18,24 +17,22 @@ public interface LogMapper {
     /**
      * 所有广告列表
      *
-     * @return a list
+     * @param adminId x
+     * @return x
      */
-    @Select("select link,name,content,pic_url,is_default,is_enabled," +
-            "start_time,end_time,gmt_create,gmt_modified,is_deleted from ad")
-    @Results(id = "ad", value = {
-            @Result(property = "link", column = "link", javaType = String.class),
-            @Result(property = "name", column = "name", javaType = String.class),
-            @Result(property = "content", column = "content", javaType = String.class),
-            @Result(property = "picUrl", column = "pic_url", javaType = String.class),
-            @Result(property = "beDefault", column = "is_default", javaType = Boolean.class),
-            @Result(property = "beEnable", column = "is_enabled", javaType = Boolean.class),
-            @Result(property = "startTime", column = "start_time", javaType = LocalDateTime.class),
-            @Result(property = "endTime", column = "end_time", javaType = LocalDateTime.class),
+    @Select("select * from log where admin_id=#{adminId}")
+    @Results(id = "log", value = {
+            @Result(property = "id", column = "id", javaType = Integer.class),
+            @Result(property = "adminId", column = "admin_id", javaType = Integer.class),
+            @Result(property = "ip", column = "ip", javaType = String.class),
+            @Result(property = "type", column = "type", javaType = Integer.class),
+            @Result(property = "action", column = "action", javaType = String.class),
+            @Result(property = "statusCode", column = "status", javaType = Integer.class),
+            @Result(property = "actionId", column = "action_id", javaType = Integer.class),
             @Result(property = "gmtCreate", column = "gmt_create", javaType = LocalDateTime.class),
-            @Result(property = "gmtModified", column = "gmt_modified", javaType = LocalDateTime.class),
-            @Result(property = "beDeleted", column = "is_deleted", javaType = Boolean.class),
+            @Result(property = "gmtModified", column = "gmt_modified", javaType = LocalDateTime.class)
     })
-    List<Ad> findAllAds();
+    List<Log> findAllLogs(Integer adminId);
 
     /**
      * 生成管理员日志
