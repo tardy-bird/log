9b7e81bb4fbe480618bc27419d7644701e07fd6c
nan <2324737911@qq.com>
2019-12-15 11:11:11 +0800

    feign

--- src/main/java/com/tardybird/log/LogApplication.java
+++ src/main/java/com/tardybird/log/LogApplication.java
@@ -3,12 +3,14 @@ package com.tardybird.log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author nick
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class LogApplication {
 
     public static void main(String[] args) {
