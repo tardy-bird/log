2f120d2e1a8faa852df2e2256a0cec5b7bb4a8a0
seekincs <seekincs@gmail.com>
2019-12-18 23:27:59 +0800

    update.

--- src/main/java/com/tardybird/log/mapper/LogMapper.java
+++ src/main/java/com/tardybird/log/mapper/LogMapper.java
@@ -1,10 +1,10 @@
 package com.tardybird.log.mapper;
 
 import com.tardybird.log.entity.Log;
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -17,29 +17,15 @@ public interface LogMapper {
     /**
      * 所有广告列表
      *
-     * @param adminId x
+     * @param id x
      * @return x
      */
-    @Select("select * from oomall_log where admin_id=#{adminId}")
-    @Results(id = "log", value = {
-            @Result(property = "id", column = "id", javaType = Integer.class),
-            @Result(property = "adminId", column = "admin_id", javaType = Integer.class),
-            @Result(property = "ip", column = "ip", javaType = String.class),
-            @Result(property = "type", column = "type", javaType = Integer.class),
-            @Result(property = "actions", column = "actions", javaType = String.class),
-            @Result(property = "statusCode", column = "status", javaType = Integer.class),
-            @Result(property = "actionId", column = "action_id", javaType = Integer.class),
-            @Result(property = "gmtCreate", column = "gmt_create", javaType = LocalDateTime.class),
-            @Result(property = "gmtModified", column = "gmt_modified", javaType = LocalDateTime.class)
-    })
-    List<Log> findAllLogs(Integer adminId);
+    List<Log> findAllLogs(@Param("id") Integer id);
 
     /**
      * 生成管理员日志
      *
      * @param log Log object
      */
-    @Insert("insert into oomall_log(admin_id,ip,actions,type,status,gmt_create,gmt_modified,action_id)  " +
-            "values(#{adminId},#{ip},#{actions},#{type},#{statusCode},now(),now(),#{actionId})")
     void addLog(Log log);
 }
